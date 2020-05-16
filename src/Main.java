0239ca0d88564a83228e2ccc2b9448953809f6b3
石塚智実 <tammy.ishizuka@gmail.com>
2020-05-15 21:11:36 -0700

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
+        double a;
+        double b;
+        double c;
+        double area;
         Scanner scanner = new Scanner(System.in);
-        int a = 0;
-        int c = 0;
-        outside: while (scanner.hasNext()) {
-            c = scanner.nextInt();
-            if (c == 0) {
-                break outside;
-            } else {
-                a += c;
-                if (a >= 1000){
-                    a = a - 1000;
-                    break outside;
-                }
-            }
+
+        String figure = scanner.next();
+
+        switch (figure) {
+            case "triangle":
+                a = scanner.nextDouble();
+                b = scanner.nextDouble();
+                c = scanner.nextDouble();
+
+                double p = (a + b + c) / 2;
+                area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+                break;
+
+            case "rectangle":
+                a = scanner.nextDouble();
+                b = scanner.nextDouble();
+                area = a * b;
+                break;
+
+            case "circle":
+                a = scanner.nextDouble();
+                area = 3.14 * a * a;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + figure);
         }
-        System.out.println(a);
+
+        System.out.print(area);
     }
 }
